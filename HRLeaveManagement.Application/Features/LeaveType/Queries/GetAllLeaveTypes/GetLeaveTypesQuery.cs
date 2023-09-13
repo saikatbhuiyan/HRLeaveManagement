27eb18893a7b6beea6b542548987d243e1a1b0e9
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-09-14 01:08:00 +0600

    Creating CQRS request 1

---
+++ HRLeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class GetLeaveTypesQuery : IRequest<List<LeaveTypeDto>>
+{
+}
+
+// public record GetLeaveTypesQuery : IRequest<List<LeaveTypeDto>>;
