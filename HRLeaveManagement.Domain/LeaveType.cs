f1659464ed2c018c07c1bd8abe90eac360234ea2
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-09-13 21:10:14 +0600

    Create the Domain project

---
+++ HRLeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿using HRLeaveManagement.Domain.Common;
+
+namespace HRLeaveManagement.Domain;
+
+public class LeaveType : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
