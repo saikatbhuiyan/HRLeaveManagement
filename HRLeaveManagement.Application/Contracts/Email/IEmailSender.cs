e691dc704da885ed813a6272e151a8dd4139b1fe
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-09-15 13:04:31 +0600

    Added thirdParty email services

---
+++ HRLeaveManagement.Application/Contracts/Email/IEmailSender.cs
@@ -0,0 +1,8 @@
+using HRLeaveManagement.Application.Models.Email;
+
+namespace HRLeaveManagement.Application.Contracts.Email;
+
+public interface IEmailSender
+{
+    Task<bool> SendEmail(EmailMessage email);
+}
--- HRLeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
+++ HRLeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HRLeaveManagement.Application.Contracts.Email;
+using HRLeaveManagement.Application.Models.Email;
+using HRLeaveManagement.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HRLeaveManagement.Infrastructure;
@@ -7,6 +10,8 @@ public static class InfrastructureServicesRegistration
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailSender, EmailSender>();
         return services;
     }
 }
